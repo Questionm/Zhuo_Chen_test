87ca4a5f21b467e753e93396f9c579e283e75223
Questionm <32825742+Questionm@users.noreply.github.com>
2019-10-23 11:50:52 -0400

    Update A-line-overlap.py

--- A-line-overlap.py
+++ A-line-overlap.py
@@ -1,9 +1,30 @@
-'''
-Question A
-
-Your goal for this question is to write a program that accepts two lines (x1,x2) and (x3,x4) on the x-axis and returns whether they overlap. 
-As an example, (1,5) and (2,6) overlaps but not (1,5) and (6,8).
-'''
+#Question A
 
 
+#code
+class Solution(object):
+  
+    # @param {list} line1
+    # @param {list} line2
+    # @return {bool}
+    
+    def isLineOverlap(self, line1, line2):
+        A, B = line1[0], line1[1]
+        E, F = line2[0], line2[1]
+        if A > F or B < E:
+            return False
+        return True		        
+        
 
+        
+#test        
+print(Solution().isLineOverlap([1,5], [2,6]))
+True
+
+
+print(Solution().isLineOverlap([1,5], [5,6]))
+True
+
+
+print(Solution().isLineOverlap([1,5], [8,10]))
+False
