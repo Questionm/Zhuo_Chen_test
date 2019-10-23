7cd21f67d16d55b78fc99df07472fd0b3b9e7071
Questionm <32825742+Questionm@users.noreply.github.com>
2019-10-23 12:31:52 -0400

    Update and rename B-string-compare.py to B-version-compare.py

--- B-version-compare.py
+++ B-version-compare.py
@@ -5,6 +5,10 @@
 
 class Solution(object):
     def compareVersion(self, version1, version2):
+        
+        #param version1
+        @param version2
+        @return
         """
         :type version1: str
         :type version2: str
@@ -15,6 +19,18 @@ class Solution(object):
 
 #general case
 
+#'0.1' < '1.1'
+#'13.2' > '1.15'
+
+
+
 #corner case
 
+#'1.0' = '1'
+#'1.15' > '1.5'
+
+
+
 #extreme case
+
+#'2..............333', '23............223.32'
