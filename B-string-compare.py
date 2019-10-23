17eab1954dde72f6eea35090032999bfdb50836c
Questionm <32825742+Questionm@users.noreply.github.com>
2019-10-23 12:04:21 -0400

    Update B-string-compare.py

--- B-string-compare.py
+++ B-string-compare.py
@@ -1,1 +1,20 @@
 #Question B
+
+
+#code
+
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+
+#test
+
+#general case
+
+#corner case
+
+#extreme case
