8ed7658eff1f5289214e80a53c926241cc164df6
Questionm <32825742+Questionm@users.noreply.github.com>
2019-10-23 21:39:45 -0400

    Update B-version-compare.py

--- B-version-compare.py
+++ B-version-compare.py
@@ -10,7 +10,27 @@ class Solution(object):
         # @param {string} version2
         # @return {int}
         
-        
+        if '..' in (version1 and version2):
+            print ('fake version string exist!')
+            
+        else:
+            
+            v1 = version1.split(".") 
+            v2 = version2.split(".") 
+
+            i = 0    
+
+            while(i < len(v1)):    
+
+                if int(v2[i]) > int(v1[i]): 
+                    return -1                             # version2 is greater
+
+                if int(v1[i]) > int(v2[i]): 
+                    return 1                              # version1 is greater
+
+                i += 1
+
+            return 0                                      # those two are equal version
       
 
 #test
@@ -24,11 +44,10 @@ class Solution(object):
 
 #corner case
 
+#'01' = '1'
 #'1.0' = '1'
 #'1.15' > '1.5'
 
 
-
 #extreme case
-
 #'2..............333', '23............223.32'
